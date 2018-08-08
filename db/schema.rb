2d5d6291e6f1cd1b032d886f11f979164be7d3ad
“babylionjane” <runinging@gmail.com>
2018-08-09 00:19:21 +0900

    이게머야
    ?ActiveRecord::Schema.define(version: 2018_08_08_035351) do
    >>>>>>> 1698331257cf1ca624e4856a3e78e2c2ba75210f

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2018_08_08_044404) do
-=======
-ActiveRecord::Schema.define(version: 2018_08_08_035351) do
->>>>>>> 1698331257cf1ca624e4856a3e78e2c2ba75210f
 
   create_table "friend_requests", force: :cascade do |t|
     t.integer "user_id"
