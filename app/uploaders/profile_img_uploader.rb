0c064c5dc7496270c05938ad3c4a03ce0f2c9b30
“babylionjane” <runinging@gmail.com>
2018-08-14 16:08:29 +0900

    commit 개많이합니더 재인 ㅠㅠ

--- app/uploaders/profile_img_uploader.rb
+++ app/uploaders/profile_img_uploader.rb
@@ -31,14 +31,19 @@ class ProfileImgUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [150, 150]
+    process resize_to_fit: [100, 100]
   end
-
+  version :thumb4 do
+    process resize_to_fit: [40, 40]
+  end
+  version :thumb3 do
+    process resize_to_fit: [30, 30]
+  end  
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
