c19a9a793c94291d867512fa222ca960331d0534
KKambi <37494867+KKambi@users.noreply.github.com>
2018-08-24 22:59:38 +0900

    Default img (#26)
    * 기본이미지

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -9,12 +9,10 @@ class TablesController < ApplicationController
 		@user = User.find(@user_id)
 		@posts = @user.posts
 		@post = Post.new
-
 	  @items = Post.where(["title LIKE ?","%#{params[:search]}%"])
 	  @friends = @user.friends
 	  @cosmetics_num = @user.posts.count
 	  @friends_num = @friends.count
-
 	end
 
 	# GET /tables/mypage
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,13 +17,69 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
-    return "/default_cos1.png" if model.category == "스킨"
+    cate_num = (model.category).to_i
 
-    return "/default_cos2.png" if model.category == "로션"
-    
-    return "/default_cos3.png" if model.category == "립스틱"
-
-    "/default_cos4.png" 
+    if cate_num <= 27  
+      #기초 메이크업
+      if cate_num <= 7
+        return "/default/gicho_toner.png" if cate_num == 2
+        return "/default/gicho_lotion.png" if cate_num == 3   
+        return "/default/gicho_essence.png" if cate_num == 4
+        return "/default/gicho_cream.png" if cate_num == 5
+        return "/default/gicho_mist.png" if cate_num == 6
+        return "/default/gicho_faceoil.png"
+      #클렌징
+      elsif cate_num <= 14
+        return "/default/cleansing_foam.png" if cate_num == 9
+        return "/default/cleansing_water.png" if cate_num == 10
+        return "/default/cleansing_oil.png" if cate_num == 11
+        return "/default/cleansing_cream.png" if cate_num == 12
+        return "/default/cleansing_remover.png" if cate_num == 13
+        return "/default/cleansing_scrub.png"
+      #베이스메이크업-기초
+      elsif cate_num <= 18
+        return "/default/basemake_gicho_makebase.png" if cate_num == 17
+        return "/default/basemake_gicho_primer.png"
+      #베이스메이크업-피부톤
+      elsif cate_num <= 23
+        return "/default/basemake_skin_cushion.png" if cate_num == 20
+        return "/default/basemake_skin_foundation.png" if cate_num == 21 
+        return "/default/basemake_skin_bb.png" if cate_num == 22
+        return "/default/basemake_skin_concealer.png" 
+      #베이스메이크업-마무리
+      else
+        return "/default/basemake_final_powder.png" if cate_num == 25
+        return "/default/basemake_final_shading.png" if cate_num == 26
+        return "/default/basemake_final_fixer.png"
+      end 
+    else
+      #색조메이크업-아이
+      if cate_num <= 33
+        return "/default/saekjo_eye_shadow.png" if cate_num == 30
+        return "/default/saekjo_eye_liner.png" if cate_num == 31
+        return "/default/saekjo_eye_brow.png" if cate_num == 32
+        return "/default/saekjo_eye_mascara.png"
+      #색조메이크업-립
+      elsif cate_num <= 37
+        return "/default/saekjo_lip_stick.png" if cate_num == 35
+        return "/default/saekjo_lip_tint.png" if cate_num == 36
+        return "/default/saekjo_lip_balm.png"
+      #색조메이크업-블러셔
+      elsif cate_num == 38
+        return "/default/saekjo_blusher.png"
+      #바디/헤어
+      elsif cate_num <= 42
+        return "/default/bodyhair_bodylotion.png" if cate_num == 40
+        return "/default/bodyhair_handlotion.png" if cate_num == 41
+        return "/default/bodyhair_hair.png" 
+      #향수
+      elsif cate_num == 43
+        return "/default/perfume.png"
+      #기타
+      else
+        return "/default/gita.png"
+      end
+    end 
   end
 
   # Process files as they are uploaded:
