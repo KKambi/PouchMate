1e51222b4981db9e21799ff2752e0e8abb43b357
yeondaeun <37509514+yeondaeun@users.noreply.github.com>
2018-08-24 20:00:49 +0900

    자잘하게 고침 (#25)

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,16 +1,20 @@
 class TablesController < ApplicationController
 	before_action :authenticate_user!
   before_action :set_user_id, only: [:index]
+   before_action :authenticate_user!
+  before_action :set_friend, only: :destroy
 
 	# GET /tables/:user_id
 	def index
 		@user = User.find(@user_id)
 		@posts = @user.posts
 		@post = Post.new
+
 	  @items = Post.where(["title LIKE ?","%#{params[:search]}%"])
 	  @friends = @user.friends
 	  @cosmetics_num = @user.posts.count
 	  @friends_num = @friends.count
+
 	end
 
 	# GET /tables/mypage
@@ -31,4 +35,7 @@ class TablesController < ApplicationController
     def set_user_id
       @user_id = params[:user_id]
     end
+    def set_friend
+    @friend = current_user.friends.find(params[:id])
+  end
 end
