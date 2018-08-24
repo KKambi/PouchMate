895cdc6b2608ffa302477ea653cb465a559b8509
root <root@DESKTOP-9S0MSU5.localdomain>
2018-08-24 15:59:15 +0900

    회원가입 모달 백 수정

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	# 디바이스 젬 그대로 모달에 복붙하면 생기는 문제 해결하는 코드
+	def resource_name
+    :user
+  end
+
+  def resource
+    @user ||= User.new
+  end
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
